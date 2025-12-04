601e68d4962579fcd45a4f05f015a4d435c027b5
Cedric <crubbertire@rogers.com>
2025-12-03 19:06:01 -0500

    documented code

--- unit-4-project-react/src/components/Nav/Nav.jsx
+++ unit-4-project-react/src/components/Nav/Nav.jsx
@@ -2,21 +2,31 @@ import "./Nav.css"
 
 const Nav = ({ handlePageChange, page}) => {
 
+  // Array containing all the pages availble for navigation.
+  // If we want to add pages, simply add a new element to the array, and it will all work seemlessly!
   const pages = ["calendar", "home", "habits"]
+
   return (
     <nav>
       <ul>
-        {pages.map((navPage, index) => (
-          <li key={index} className={page == navPage ? "active" : ""}>
+        {pages.map((navPage, index) => ( // Loop through the array of pages
+          <li key={index} className={page == navPage ? "active" : ""}> {/* Set active if this matches current page */}
             <button className="nav-link" onClick={() => { handlePageChange(navPage) }}>
-              <img src={page == navPage ? `/icons/${navPage}-accent.svg` : `/icons/${navPage}-dark.svg`} alt={`${navPage} page`} className="icon icon-nav" />
+              <img 
+                src={page == navPage ? `/icons/${navPage}-accent.svg` : `/icons/${navPage}-dark.svg`} /* Based on the current page, set the icon to the page's icon + colorstate */
+                alt={`${navPage} page`} 
+                className="icon icon-nav" 
+              />
               <p>{navPage}</p>
             </button>
           </li>
         ))}
       </ul>
+      
     </nav>
+  
   )
 }
+
 
 export default Nav;
